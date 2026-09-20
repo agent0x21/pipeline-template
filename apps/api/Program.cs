8185160972d44e77fd870d60f97f7e6ae3ea9611
agent0x21 <joncuyler@gmail.com>
2026-09-20 14:30:12 +0200

    fix: remove 'humid-test' from weather summary options

--- apps/api/Program.cs
+++ apps/api/Program.cs
@@ -21,7 +21,7 @@ app.UseCors("web");
 
 var summaries = new[]
 {
-    "Freezing", "humid-test", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Blazing", "Scalding"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Blazing", "Scalding"
 };
 
 app.MapGet("/weatherforecast", () =>
